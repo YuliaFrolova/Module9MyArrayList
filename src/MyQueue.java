c37ac58171204db233152043e45a22735edcc10c
yulia <vaydafox@gmail.com>
2023-06-22 13:01:49 +0300

    Added class MyQueue

---
+++ src/MyQueue.java
@@ -0,0 +1,28 @@
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MyQueue {
+    public static void main(String[] args) {
+        Queue<Integer> numbers = new PriorityQueue<>();
+        numbers.add(16);
+        numbers.add(117);
+        numbers.add(9872567);
+        numbers.add(9273);
+        int size = numbers.size();
+        System.out.println(size);
+        int firstElement = numbers.peek();
+        System.out.println("FirstElement: " + firstElement);
+        int deleteFirstElement = numbers.poll();
+        System.out.println(deleteFirstElement);
+        System.out.println(numbers);
+        numbers.clear();
+        System.out.println(numbers);
+
+
+
+
+
+            }
+        }
+
